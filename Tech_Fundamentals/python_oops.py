1607b9de630e93fd8a739f7cbd5b6186190d9308
Alen Kuriakose <55983813+alen-kuriakose@users.noreply.github.com>
2022-08-05 11:49:58 +0530

    Update python_oops.py

--- Tech_Fundamentals/python_oops.py
+++ Tech_Fundamentals/python_oops.py
@@ -1,22 +1,21 @@
-
-class Cars():
-    def __init__(self, modelname, yearm, price):
+class Cars(object):
+    def __init__(self, modelname, yearm, price,__avgcarprice):
         self.modelname = modelname
         self.yearm = yearm
         self.price = price
+        self.__avgcarprice = 700000
 
     def price_inc(self):
         self.price = int(self.price*1.5)
 
 
 class Supercar(Cars):
-    def __init__(self, modelname, yearm, price):
-        super.__init__(modelname, yearm, price)
-        # self.cc = cc
+    def __init__(self, modelname, yearm, price,__avgcarprice,carcc):
+        self.carcc = carcc
+        Cars.__init__(self,modelname,yearm,price,__avgcarprice)
 
 
-honda = Supercar("city", 2010, 200000)
-honda.cc=1200
+honda = Supercar("city", 2010, 200000,300000,1200)
 print(honda.__dict__)
 honda.price_inc()
 print(honda.price)
