31fd83da5b09a84b9647299229bf57baf279927d
Alen Kuriakose <alenkuriakosecpcsk007@gmail.com>
2022-08-09 01:20:02 +0530

    changes flask app

--- Flask/server.py
+++ Flask/server.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='template')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 # main driver function
