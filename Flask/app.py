ce3d2cf80883bc8322f026cac3337d06f2e66683
Alen Kuriakose <alenkuriakose@ti-lap-248.idc.tarento.com>
2022-08-12 15:46:13 +0530

    completed website

--- Flask/app.py
+++ Flask/app.py
@@ -1,25 +1,26 @@
-from crypt import methods
-from urllib import request
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-app = Flask(__name__, template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-db=SQLAlchemy(app)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    content=db.Column(db.String(200),nullable=False)
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' %self.id
+        return '<Task %r>' % self.id
 
-@app.route('/', methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
+
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -29,7 +30,8 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', task=tasks)
+        return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -59,6 +61,5 @@ def update(id):
         return render_template('update.html', task=task)
 
 
-# main driver function
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
